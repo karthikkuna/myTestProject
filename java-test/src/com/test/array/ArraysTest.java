839c3872cb3cc207777030a5b54035c799af3f15
Karthik <karthikkuna@gmail.com>
2016-10-02 21:06:59 +0530

    Intaial Commit

---
+++ java-test/src/com/test/array/ArraysTest.java
@@ -0,0 +1,23 @@
+package com.test.array;
+
+public class ArraysTest {
+public static void main(String... arg){
+//	System.out.println("fucking");
+//String mod=System.getProperty("sun.arch.data.model") ;
+//System.out.println("mod "+mod);
+//	String str = "abc";
+//	String repeated = new String(new char[5]).replace("\0", str);
+//	 System.out.println(repeated);
+//	Mockito
+	
+	System.out.println("main method");
+//	List<>
+}
+static {
+	System.out.println("Static block");
+
+}
+{
+	System.out.println("Block");
+}
+}
